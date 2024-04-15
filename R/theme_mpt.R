47eba115e091b215be6f2c613cf2fd5c1f520ba7
juan <>
2024-04-15 09:01:38 -0300

    arreglado para funcionar como outlier

--- R/theme_mpt.R
+++ R/theme_mpt.R
@@ -12,6 +12,12 @@
 #' @examples ggplot() + theme_mpt() -> Establece los parámetros al theme
 #'
 theme_mpt <- function(){
+  .pie <<- "Elaborado"
+  .paleta <<- c("#9CD6F9", "#757679", "#14366D", "#235DBC" , "#4CAAE2", "#0C1F3E",
+                  "#404042",  "#063970", "#4CAAE2" ,
+                  "#235DBC", "#1C4993",
+                  "#C6D6EE",
+                  "#CAD1DC" , "#939599")
   tidyquant::theme_tq() %+replace%
     theme(
       panel.grid.major.x = element_blank(),
